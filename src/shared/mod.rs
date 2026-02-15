2280a5f9883b98b709ddeaa206288291742d434f
James Garfield <jordan@grupojvm.com>
2026-02-15 00:44:05 -0500

    *Add int values to runtime

--- src/shared/mod.rs
+++ src/shared/mod.rs
@@ -217,21 +217,20 @@ pub trait PixelScript {
 #[allow(non_camel_case_types)]
 pub enum pxs_Runtime {
     /// Lua v5.4 with mlua.
-    pxs_Lua,
+    pxs_Lua = 0,
     /// Python v3.x with pocketpy.
-    pxs_Python,
+    pxs_Python = 1,
     /// ES 2020 using rquickjs
-    pxs_JavaScript,
+    pxs_JavaScript = 2,
     /// v0.4.5 using easyjsc
-    pxs_Easyjs,
+    pxs_Easyjs = 3,
     /// Python >= v3.8 with RustPython
-    pxs_RustPython,
+    pxs_RustPython = 4,
     /// PHP v5.3 with PH7
-    pxs_PHP
+    pxs_PHP = 5
 }
 
 impl pxs_Runtime {
-    ///
     pub fn from_i64(val: i64) -> Option<Self> {
         match val {
             0 => Some(Self::pxs_Lua),
